31c8d7bb950d18d5f2762534de36c343c8d77ac2
Aris13lo <Aris13lo@gmail.com>
2023-10-25 16:16:25 -0300

    Actualizacion

---
+++ src/helpers/cursoApi.js
@@ -0,0 +1,17 @@
+const url = "http://localhost:8080/api/cursos";
+const token = JSON.parse(localStorage.getItem("token"));
+
+//Traer cursos
+export const getCursos = async (limite = 0, pagina = 0) => {};
+
+//Traer cursos por el id
+export const getCursoById = async (id) => {};
+
+//crear un curso
+export const crearCurso = async (datos) => {};
+
+//Actualizar un curso
+export const actualizarCurso = async (id, datos) => {};
+
+//Borrar un curso
+export const borrarCurso = async (id) => {};
