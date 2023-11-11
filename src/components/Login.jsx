3e6883602bd2ec0d32c425a76065d5344c30d68d
Sergio Ocampo <sergioocampo1@gmail.com>
2023-11-11 10:01:14 -0300

    se realizaron cambios en el contenido de los formularios y en su estilo

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,9 @@
 
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 // import { useNavigate } from "react-router-dom";
 import { authLogin } from "../helpers/ApiLogin";
-import MessageApp from "../components/MessageApp";
+//import MessageApp from "../components/MessageApp";
 import "../css/login.css";
 
 
@@ -44,6 +44,56 @@ const Login = ({ iniciarSesion, guardarUsuario, registro }) => {
   return (
     <>
       <div>
+      <div className="bg-dark">
+      <div className="container container-login">
+        <div className="row px-2">
+          <div className="col-12 col-md-4 offset-md-4 card-login">
+            <div className="d-flex justify-content-center align-items-center">
+              <img src={logo} alt="logo" />
+            </div>
+            <h3 className="text-center mt-2">
+              <span>
+                <i className="fa fa-user-circle" aria-hidden="true"></i>
+              </span>
+              Inicio de sesión
+            </h3>
+            <form onSubmit={handleLogin}>
+              <div className="mt-3">
+                <label className='sr-only'>User</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  value={inputCorreo}
+                  onChange={(e) => setInputCorreo(e.target.value)}
+                />
+              </div>
+              <div className="mt-3">
+                <label className='sr-only' >Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  value={inputPassword}
+                  onChange={(e) => setInputPassword(e.target.value)}
+                />
+              </div>
+              <div className="mt-3 d-grid">
+                <button className="btn btn-dark" disabled={loading && true}>
+                  Iniciar Sesion
+                </button>
+                <p className='error escondido' >Error al iniciar sesion</p>
+              </div>
+            </form>
+            <p>¿Todavia no tenes una cuenta? <Navlink to="/register">Registrate</Navlink> </p>
+            {/* ternario para mostrar mensaje con el "resultado" */}
+            {resultado?.msg && (
+              <div className="mt-2">
+                <MessageApp mensaje={resultado.msg} />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
       </div >
     </>
   )
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,103 +1,162 @@
 
-import React from 'react'
-import { NavLink } from "react-router-dom";
-import { useForm } from "react-hook-form"
-import { edadValidator } from './Validator';
-import { Registrar } from "../helpers/ApiRegister"
-import { validarTexto } from '../validators/validarTexto';
-import Swal from 'sweetalert';
-import "../css/Login.css"
+//import Swal from 'sweetalert';
+import "../css/Register.css"
+import { Link, NavLink } from "react-router-dom";
+import { Form, Button, Row, Col, Card } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert";
 
+const App = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }, reset
+  } = useForm();
 
-const Register = () => {
-    const { register, formState: {errors}, watch , handleSubmit } = useForm({
-        defaultValues: {
-            nombre: "Luis",
-            direccion: "Calle Gral Paz"
-        }
-    });
-
-    const onSubmit = (data) => {
-        console.log(data);
-
-    }
-
-    const incluirTelefono = watch("incluirTelefono");
-
-  
+  const onSubmit = (data) => {
+    console.log(data);
+    Swal.fire(
+      "Datos enviados",
+      "Los datos fueron enviados correctamente",
+      "success"
+    );
+    reset();
+  };
 
 
 
   return (
     <>
-        <div className="form-container" >
-        <h1>Crear cuenta</h1>
+    <div className='bg-dark'>
+      <div className='container container-register'>
+        <div className='row px-2' >
+        <div className='card-register'>
+        <Card border="primary">
+      <Card.Header className="bg-primary text-light card-titulo">
+      <h1>Crear cuenta</h1>
           <p>Crea tu cuenta para entrar al area de administracion</p>
-        <p>Nombre: {watch("nombre")} </p>
-        <form  onSubmit={handleSubmit(onSubmit)} >
-            <div className="input-box" >
-                <label className='sr-only' >Nombre</label>
-                <input type="text" {...register("nombre", {
-                    required: true,
-                    maxLength: 10
-                })} />
-                {errors.nombre?.type ==="required" && <p>El campo nombre es requerido</p> } 
-                {errors.nombre?.type ==="maxLength" && <p>El campo nombre debe tener menos de 10 caracteres</p> } 
-
-            </div>
-            <div className="input-box" >
-                <label className='sr-only' >Dirección</label>
-                <input type="text" {...register("dirección", {
-                    required: true,
-                })} />
-            </div>
-            <div className="input-box" >
-                <label className='sr-only' >Email</label>
-                <input type="text" {...register("email", {
-                    pattern: /\S+@\S+\.\S+/
-                })} />
-                { errors.email?.type === "pattern" && <p>El formato del email es incorrecto</p> }
-            </div>
-
-            <div className="input-box" >
-                <label className='sr-only' >Edad</label>
-                <input type="text" {...register("edad",{
-                    validate: edadValidator
-                })} />
-                {errors.edad && <p>La edad debe estar entre 18 y 65</p> }
-            </div>
-
-            <div className="input-box" >
-                <label className='sr-only' >Pais</label>
-                <select {...register("país")} >
-                    <option value="ar">Argentina</option>
-                    <option value="ch">Chile</option>
-                    <option value="ur">Uruguay</option>
-                </select>
-            </div>
-            <div className="input-box" >
-                <label className='sr-only' >¿Incluir telefono?</label>
-                <input type="checkbox" {...register("incluirTelefono")} />
-            </div>
-            {incluirTelefono && (
-            <div className="input-box" >
-            <label className='sr-only' >Telefono</label>
-            <input type="text" {...register("telefono")} />
-            </div>
-
-            )}
-
-            {/* <input type="submit" value="Enviar" /> */}
-            <button type='=submit' >Iniciar Sesion</button>
-            <p className='error escondido' >Error al registrarse</p>
-
-        </form>
-        <p>¿Todavia no tenes una cuenta? <navlink to="/">Iniciar sesion</navlink> </p>
+      </Card.Header>
+      <Card.Body>
+        <Form noValidate onSubmit={handleSubmit(onSubmit)}>
+          <Row className="mb-3">
+            <Form.Group
+              as={Col}
+              md="6"
+              controlId="validationNombre"
+              className="mb-3"
+            >
+              <Form.Label className='sr-only'  >Nombre</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Juan"
+                {...register("nombre", {
+                  required: "El nombre es obligatorio",
+                  minLength: {
+                    value: 2,
+                    message: "La cantidad minima de caracteres es 2",
+                  },
+                  maxLength: {
+                    value: 50,
+                    message: "La cantidad maxima de caracteres es 50",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.nombre?.message}
+              </Form.Text>
+            </Form.Group>
+            <Form.Group
+              as={Col}
+              md="6"
+              controlId="validationApellido"
+              className="mb-3"
+            >
+              <Form.Label className='sr-only'  >Apellido</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Perez"
+                {...register("apellido", {
+                  required: "El apellido es obligatorio",
+                  minLength: {
+                    value: 2,
+                    message: "La cantidad minima de caracteres es 2",
+                  },
+                  maxLength: {
+                    value: 50,
+                    message: "La cantidad maxima de caracteres es 50",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.apellido?.message}
+              </Form.Text>
+            </Form.Group>
+            <Form.Group
+              as={Col}
+              md="6"
+              controlId="validationDNI"
+              className="mb-3"
+            >
+              <Form.Label className='sr-only'  >DNI</Form.Label>
+              <Form.Control
+                required
+                type="number"
+                placeholder="9999999"
+                {...register("dni", {
+                  required: "El dni es obligatorio",
+                  pattern: {
+                    value: /^\d{7,8}(?:[-\s]\d{4})?$/,
+                    message: "Debe ingresar un dni valido de 7 a 8 caracteres",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.dni?.message}
+              </Form.Text>
+            </Form.Group>
+            <Form.Group
+              as={Col}
+              md="6"
+              controlId="validationEmail"
+              className="mb-3"
+            >
+              <Form.Label className='sr-only'  >Email</Form.Label>
+              <Form.Control
+                required
+                type="email"
+                placeholder="juanperez@mail.com"
+                {...register("email", {
+                  required: "El email es obligatorio",
+                  pattern: {
+                    value:
+                      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                    message: "Debe ingresar un email valido",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.email?.message}
+              </Form.Text>
+            </Form.Group>
+          </Row>
+          <Button type="submit">Iniciar Sesion</Button>
+          <p className='error escondido' >Error al registrarse</p>
+        </Form>
+        <p>¿Todavia no tenes una cuenta? <navlink to="/Login">Iniciar sesion</navlink> </p>
+      </Card.Body>
+    </Card>
+        </div>
+        </div>
+      </div>
     </div>
-
-
     </>
+
   )
 }
 
-export default Register;
+export default App;
+
+
+
