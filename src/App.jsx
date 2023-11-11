374473bc92f04cdafa0725848493e3a69ed99f04
Sergio Ocampo <sergioocampo1@gmail.com>
2023-11-11 11:18:28 -0300

    se hicieron las rutas de reserva

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,47 @@
 
-import { useState } from "react";
+//import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ProtectedRoutes from "./routes/ProtectedRoutes";
-import LoginScreen from "./pages/LoginScreen";
-import RoutesDos from "./routes/RoutesDos";
+//import ProtectedRoutes from "./routes/ProtectedRoutes";
+import Reservas from "./pages/Reservas";
+//import RoutesDos from "./routes/RoutesDos";
 
 // import ErrorScreen from "./pages/ErrorScreen";
 
 function App() {
-  //Estados para manejar el login y datos del usuario
-  const [login, setLogin] = useState(false);
-  const [user, setUser] = useState(null);
+  // //Estados para manejar el login y datos del usuario
+  // // const [login, setLogin] = useState(false);
+  // // const [user, setUser] = useState(null);
 
-  //funcion para guarsar datos del usuario
-  const guardarUsuario = (datos) => {
-    setUser(datos);
-  };
+  // //funcion para guarsar datos del usuario
+  // // const guardarUsuario = (datos) => {
+  // //   setUser(datos);
+  // };
 
-  //funcion para iniciar sesion
-  const iniciarSesion = () => {
-    setLogin(true);
-  };
+  // //funcion para iniciar sesion
+  // // const iniciarSesion = () => {
+  // //   setLogin(true);
+  // };
 
-  //funcion para cerrar sesion
-  const cerrarSesion = () => {
-    setLogin(false);
-  };
+  // //funcion para cerrar sesion
+  // const cerrarSesion = () => {
+  //   setLogin(false);
+  // };
 
   return (
     <BrowserRouter>
       <Routes>
         {/* Rutas proteg para login, datos de usuario y func cerrar sesion */}
-        <Route
+        {/* <Route
           path="/*"
           element={
             <ProtectedRoutes login={login}>
               <RoutesDos cerrarSesion={cerrarSesion} user={user} />
             </ProtectedRoutes>
           }
-        />
-        <Route path="*" element={<ErrorScreen />} />
+        /> */}
+        <Route path="/Reservas" element={<Reservas />} />
 
-        {/* Ruta login que recibe funcion iniciar sesion y guardar datos */}
+        {/* Ruta login que recibe funcion iniciar sesion y guardar datos
         <Route
           path="/login"
           element={
@@ -50,7 +50,7 @@ function App() {
               guardarUsuario={guardarUsuario}
             />
           }
-        />
+        /> */}
       </Routes>
     </BrowserRouter>
   );
--- src/routes/RoutesDos.jsx
+++ src/routes/RoutesDos.jsx
@@ -1,32 +1,32 @@
-import { Routes, Route } from "react-router-dom";
-import FooterApp from "../components/FooterApp";
-import NavbarApp from "../components/NavbarApp";
-import AboutScreen from "../pages/AboutScreen";
-import AdminScreen from "../pages/AdminScreen";
-import ErrorScreen from "../pages/ErrorScreen";
-import HomeScreen from "../pages/HomeScreen";
-import ProtectedRoutesAdmin from "../routes/ProtectedRoutesAdmin";
+// import { Routes, Route } from "react-router-dom";
+// import FooterApp from "../components/FooterApp";
+// import NavbarApp from "../components/NavbarApp";
+// import AboutScreen from "../pages/AboutScreen";
+// import AdminScreen from "../pages/AdminScreen";
+// import ErrorScreen from "../pages/ErrorScreen";
+// import HomeScreen from "../pages/HomeScreen";
+// import ProtectedRoutesAdmin from "../routes/ProtectedRoutesAdmin";
 
-const RoutesDos = ({ cerrarSesion, user }) => {
-  return (
-    <>
-      <NavbarApp cerrarSesion={cerrarSesion} user={user} />
-      <Routes>
-        <Route path="/" element={<HomeScreen />} />
-        <Route path="/about" element={<AboutScreen />} />
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoutesAdmin user={user}>
-              <AdminScreen />
-            </ProtectedRoutesAdmin>
-          }
-        />
-        <Route path="*" element={<ErrorScreen />} />
-      </Routes>
-      <FooterApp />
-    </>
-  );
-};
+// const RoutesDos = ({ cerrarSesion, user }) => {
+//   return (
+//     <>
+//       <NavbarApp cerrarSesion={cerrarSesion} user={user} />
+//       <Routes>
+//         <Route path="/" element={<HomeScreen />} />
+//         <Route path="/about" element={<AboutScreen />} />
+//         <Route
+//           path="/admin"
+//           element={
+//             <ProtectedRoutesAdmin user={user}>
+//               <AdminScreen />
+//             </ProtectedRoutesAdmin>
+//           }
+//         />
+//         <Route path="*" element={<ErrorScreen />} />
+//       </Routes>
+//       <FooterApp />
+//     </>
+//   );
+// };
 
-export default RoutesDos;
+// export default RoutesDos;
