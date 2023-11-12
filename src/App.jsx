8f92d698c3000f1379e84a4a0dcc18fa9bf37f3f
Sergio Ocampo <sergioocampo1@gmail.com>
2023-11-12 20:16:30 -0300

    se crearon rutas y se instalaron dependencias, tembien se limpio la rama

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import './App.css'
-import './pages/HomeScreen'
-import HomeScreen from './pages/HomeScreen'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RoutesApp from "./routes/RoutesDos";
 
 function App() {
-
   return (
     <>
-      <HomeScreen/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<RoutesApp />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
