c8a311dd817dd49181a333c0cdd727a662f21d2d
219WD <macgunmanmc@gmail.com>
2023-11-13 23:16:22 -0300

    elimine gsap y anime

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,98 +1,13 @@
-import React, { useEffect } from 'react';
-import anime from 'animejs';
-import { gsap } from "gsap";
+import React from 'react';
 import '../css/HomeScreen.css';
 import header from '../assets/header.jpg';
 import hero from '../assets/hero.jpg';
 
 function HomeScreen() {
-  useEffect(() => {
-    var textWrapper = document.querySelector(".loader-text");
-    textWrapper.innerHTML = textWrapper.textContent.replace(
-      /\S/g,
-      "<span class='letter'>$&</span>"
-    );
-
-    anime.timeline().add({
-      targets: ".loader-text .letter",
-      translateX: [40, 0],
-      translateZ: 0,
-      opacity: [0, 1],
-      easing: "easeOutExpo",
-      duration: 1200,
-      delay: (el, i) => 2400 + 30 * i,
-    });
-
-    gsap.from(".loader-wrapper", 2, {
-      scale: 0.8,
-      ease: "power1.inOut",
-    });
-
-    gsap.from(".loader", 2, {
-      top: "100%",
-      ease: "power3.inOut",
-    });
-
-    gsap.to(
-      ".loader-wrapper, .pre-loader",
-      1,
-      {
-        scale: 1.5,
-        opacity: 0,
-        display: "none",
-        ease: "power3.inOut",
-        delay: 2,
-      },
-      "-=1"
-    );
-
-    gsap.from(".revealer", 1, {
-      width: "0%",
-      ease: "power3.inOut",
-      delay: 3,
-    });
-
-    gsap.to(".revealer", 0.8, {
-      left: "100%",
-      ease: "power3.inOut",
-      delay: 4,
-    });
-
-    gsap.to(".loader-text-wrapper", 0.2, {
-      opacity: 0,
-      display: "none",
-      ease: "power3.inOut",
-      delay: 4,
-    });
-
-    gsap.from(".col-left", 1, {
-      left: "-100%",
-      ease: "power3.inOut",
-      delay: 4.5,
-    });
-
-    gsap.from(".col-left .copy > div", 1.5, {
-      x: "-40",
-      opacity: 0,
-      ease: "power3.inOut",
-      delay: 5,
-      stagger: {
-        amount: 0.3,
-      },
-    });
-
-    gsap.from(".col-right .header", 1, {
-      x: "-20",
-      opacity: 0,
-      ease: "power3.inOut",
-      delay: 5.5,
-    });
-  }, []);
-
   return (
     <div>
       <div className="hero-img">
-      <img src={hero} alt="" />
+        <img src={hero} alt="" />
       </div>
       <div className="container">
         <div className="cols">
@@ -103,7 +18,7 @@ function HomeScreen() {
                 Solicitar una reservación para una mesa
               </div>
               <div className="img-wrapper">
-              <img src={header} alt="" />
+                <img src={header} alt="" />
               </div>
               <div className="info">
                 Bienvenidos a El Vesubio: Donde la Pasión por la Gastronomía Alcanza las Estrellas. Le invitamos a unirse a nosotros en El Vesubio para experimentar un festín culinario sin igual. Para solicitar una reservación o reservar una mesa.
@@ -127,38 +42,9 @@ function HomeScreen() {
             </div>
           </div>
         </div>
-        <nav>
-          <div className="nav-items">
-            <div className="nav-item">
-              <a href="#">acerca de nosotros</a>
-            </div>
-            <div className="nav-item">
-              <a href="#">nuestro hotel</a>
-            </div>
-          </div>
-          <div className="nav-items">
-            <div className="nav-item">
-              <a href="#" id="logo">Vesubio &copy;</a>
-            </div>
-          </div>
-          <div className="nav-items">
-            <div className="nav-item">
-              <a className="nav-link-right" href="#">iniciar sesion</a>
-            </div>
-            <div className="nav-item">
-              <a className="nav-link-right" href="#">reserva</a>
-            </div>
-          </div>
-        </nav>
       </div>
       <div className="loader-text-wrapper block">
         <h1 className="loader-text">Vesubio</h1>
-      </div>
-      <div className="revealer"></div>
-      <div className="pre-loader block">
-        <div className="loader-wrapper">
-          <div className="loader"></div>
-        </div>
       </div>
     </div>
   );
